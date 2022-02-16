fa7384136f18792bf87fbedd7e5300fbe62a3f66
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-16 15:49:27 +0300

    login interface.

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <!--Title-->
+    <title>login-Online Voting System</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <meta name="author" content="Gideon Kiplangat"/>
+
+    <!--  Description  -->
+    <meta name="description" content="userinterface, voting system, voter,"/>
+    <!-- Keywords  -->
+    <meta name="keywords" content="form, container, header, footer">
+    <!-- Font-awesome Link -->
+    <link rel="stylesheet" href="css/all.min.css">
+    <!--CSS link -->
+    <link rel="stylesheet" href="assets/css/main.css">
+</head>
+<body>
+    
+</body>
+</html>
