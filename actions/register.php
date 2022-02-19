671865d86ebb9b24e93e63a726bb41b07f3a76c5
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-19 16:09:30 +0300

    uploads
    created  upload folder
    updates on the register.php

--- actions/register.php
+++ actions/register.php
@@ -1,12 +1,12 @@
 <?php
 include ('connect.php');
 
-$username = $_POST['username']; 
-$mobile = $_POST['mobile']; 
-$password = $_POST['password']; 
-$cpassword = $_POST['cpassword']; 
-$image = $_POST['photo']['name']; 
-$tmp_name = $_POST['photo']['tmp_name']; 
+$username = $_POST['username'];
+$mobile = $_POST['mobile'];
+$password = $_POST['password'];
+$cpassword = $_POST['cpassword'];
+$image = $_FILES['photo']['name'];
+$tmp_name = $_FILES['photo']['tmp_name'];
 $std = $_POST['std']; 
 
 if($password!=$cpassword){
@@ -14,5 +14,19 @@ if($password!=$cpassword){
     alert("Passwords did not match");
     window.location="../partials/registration.php"
     </script>';
+} else {
+    move_uploaded_file($tmp_name, "../uploads/$image");
+    $sql = "INSERT INTO userdata (username,mobile,password,photo,standard,status,votes)VALUES ('$username','$mobile','$password','$image','$std',0,0)";
+
+    $result = mysqli_query($con, $sql);
+    if ($result) {
+        echo '<script>
+        alert("Wooow! Registration Successfully");
+        window.location="../index.php";
+        </script>';
+    } else {
+        echo "Error";
+    }
+    
 }
 ?>
