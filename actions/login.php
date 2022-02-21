3e9730749ed7b7cb799e5a0538cad8c53d94dcbc
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-21 10:41:41 +0300

    updates
    login.php
    dashboard.php

--- actions/login.php
+++ actions/login.php
@@ -4,18 +4,27 @@ include ('connect.php');
 $username = $_POST['username'];
 $mobile = $_POST['mobile'];
 $password = $_POST['password'];
-$std = $_POST['std']; 
+$std = $_POST['std'];
 
-$sql="SELECT * FROM userdata WHERE username='$username'AND
-mobile='$mobile' AND password=''$password AND standard='$std'";
+$sql = "SELECT * FROM userdata WHERE username='$username'AND
+mobile='$mobile' AND password='$password' AND standard='$std'";
 
 $result=mysqli_query($con, $sql);
 if(mysqli_num_rows($result)>0){
-    $sql = "SELECT username, photo, votes,id FROM userdata WHERE standard='group'";
+    $sql = "SELECT username,photo,votes,id FROM userdata WHERE standard='group'";
     $resultgroup=mysqli_query($con, $sql);
     if(mysqli_num_rows($resultgroup)>0){
         $groups=mysqli_fetch_all($resultgroup, MYSQLI_ASSOC);
+        $_SESSION['groups'] = $groups;
     }
+    $data = mysqli_fetch_array($result);
+    $_SESSION['id'] = $data['id'];
+    $_SESSION['status'] = $data['status'];
+    $_SESSION['data'] = $data;
+
+    echo '<script>
+        window.location="../partials/dashboard.php";
+        </script>';
 }else{
     echo '<script>
         alert("Invalid Credentials");
