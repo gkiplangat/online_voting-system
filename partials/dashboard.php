9280d7d1c7007d667d79b8d8f301077bade1416a
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-21 15:23:32 +0300

    dashboard
    groups

--- partials/dashboard.php
+++ partials/dashboard.php
@@ -36,17 +36,26 @@ if($_SESSION['status']==1){
         <h1 class="my-3">Online Voting System</h1>
         <div class="row my-5">
             <div class="col-md-7">
+                <?php
+                if(isset($_SESSION['groups'])){
+                    $groups=$_SESSION['groups'];
+                    for($i=0; $i<count($groups); $i++){
+                        ?>
                 <!--Groups-->
                 <div class="row">
                     <div class="col-md-4">
-                        <img src="" alt=" Group Image">
+                        <img src="../uploads/<?php echo $groups[$i]['photo']; ?>" class="img-circle" alt=" Group Image">
                     </div>
                     <div class="col-md-8">
-                        <strong class="text-dark h5">Group Name: </strong><br> <br>
-                        <strong class="text-dark h5">Votes: </strong><br> <br>
+                        <strong class="text-dark h5">Group Name: </strong><?php echo $groups[$i]['username'];?><br> <br>
+                        <strong class="text-dark h5">Votes: </strong><?php echo $groups[$i]['votes'];?><br> <br>
                     </div>
                 </div>
                 <hr>
+                <?php
+                }
+                }
+                ?>
             </div>
             <div class="col-md-5">
                 <!--User profile-->
@@ -58,20 +67,20 @@ if($_SESSION['status']==1){
                 <strong class="text-dark h5">Status: </strong><?php echo $status; ?><br> <br>
 
             </div>
+
         </div>
-    </div>
 
 
-    <!--Javascript Libraries-->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-        integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
-    </script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
-        integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
-    </script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-        integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
-    </script>
+        <!--Javascript Libraries-->
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
+            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"
+            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous">
+        </script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
+            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous">
+        </script>
 </body>
 
 </html>
