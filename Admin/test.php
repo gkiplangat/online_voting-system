386894a95c58c0e6693471e6caab499b65fcac5c
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-02-24 20:38:12 +0300

    new files

---
+++ Admin/test.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body class="bg-dark">
+    <div class="container bg-info text-center">
+        <form action="" method="POST" class="login-email">
+            <p class=" text-dark login-text" style="font-size: 2rem; font-weight:800;">Login</p>
+            <div class="input-group">
+                <input type="email" placeholder="Email" name="email" required>
+            </div>
+            <div class="input-group">
+                <input type="password" placeholder="Password" name="password" required>
+            </div>
+            <div class="input-group">
+                <button name="submit" class="btn btn-dark">Login</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
--- actions/login.php
+++ actions/login.php
@@ -11,16 +11,6 @@ $sql = "SELECT * FROM userdata WHERE email='$email' AND password='$password'";
 
 $result=mysqli_query($con, $sql);
 if(mysqli_num_rows($result)>0){
-    $sql = "SELECT username,photo,votes,id FROM userdata WHERE standard='group'";
-    $resultgroup=mysqli_query($con, $sql);
-    if(mysqli_num_rows($resultgroup)>0){
-        $groups=mysqli_fetch_all($resultgroup, MYSQLI_ASSOC);
-        $_SESSION['groups'] = $groups;
-    }
-    $data = mysqli_fetch_array($result);
-    $_SESSION['id'] = $data['id'];
-    $_SESSION['status'] = $data['status'];
-    $_SESSION['data'] = $data;
 
     echo '<script>
         window.location="dashboard/index.php";
