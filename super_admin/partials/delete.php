62c95ded9a5008356e50143fa2a427cbcc8f27db
giddy22-02 <56033401+giddy22-02@users.noreply.github.com>
2022-03-10 14:59:30 +0300

    super-admin section

---
+++ super_admin/partials/delete.php
@@ -0,0 +1,22 @@
+<?php 
+//===Connection to the Database====================
+include ('../../actions/connect.php');
+//Session Start
+session_start();
+if(isset($_POST['delete']))
+{
+    $id = $_POST['id'];
+
+    $query = "DELETE FROM candidates WHERE id ='$id'";
+    $query_run = mysqli_query($con, $query);
+
+    if($query_run){
+         $_SESSION['status'] = "Deleted Successfully";
+         header("Location: ../candidates.php");
+    }else
+    {
+        $_SESSION['status'] = "Data not Deleted";
+        header("Location: ../candidates.php");
+    }
+}
+?>
--- super_admin/secretary.php
+++ super_admin/secretary.php
@@ -35,45 +35,21 @@ if(!isset($_SESSION['username'])){
             <!--offcanvas trigger-->
             <button class="navbar-toggler me-2" type="button" data-bs-toggle="offcanvas"
                 data-bs-target="#offcanvasExample" aria-controls="offcanvasExample">
-                <span class="navbar-toggler-icon" data-bs-target="#offcanvasExample"></span>
+                <span class="  navbar-toggler-icon" data-bs-target="#offcanvasExample"></span>
             </button>
             <!--offcanvas trigger-->
             <!---Logo------------------------->
-            <img src="../Admin/dashboard/images/logo.png" alt="logo" width="60" height="60">
+            <img src="../Admin/dashboard/images/logo.png" alt="logo" width ="50" height="50">
             <!---Logo------------------------->
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
                 aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
+                <span class=" navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-
-                <form class="d-flex ms-auto">
-                    <div class="input-group my-3 my-lg-0">
-                        <input type="text" class="form-control" placeholder="Recipient's username"
-                            aria-label="Recipient's username" aria-describedby="button-addon2">
-                        <button class="btn btn-primary" type="button" id="button-addon2"><i
-                                class="bi bi-search"></i></button>
-                    </div>
-                </form>
+            <div class="  collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav   mb-2 mb-lg-0">
-
-
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="bi bi-person-fill"></i>
-                        </a>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li>
-                                <hr class="dropdown-divider">
-                            </li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-
+                    <a href="../logout.php" role="button" class="btn btn-Info text-info fw-bold"> <i
+                            class="bi bi-person-fill "></i>LOGOUT</a>
                 </ul>
             </div>
         </div>
